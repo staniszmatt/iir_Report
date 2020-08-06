354cdc8ce287970808bb0e776edd723534384672
staniszmatt <matthewpstaniszewski@gmail.com>
2020-08-06 08:27:26 -0600

    some what working version for now

--- app/components/tearDownSummery.tsx
+++ app/components/tearDownSummery.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { PDFViewer, PDFDownloadLink } from '@react-pdf/renderer';
 import ReactDOM from 'react-dom';
 import TearDownPDF from './tearDowSummeryPDF';
-import Btn from './buttonFunctions/buttonClickHandler';
-// import styles from './tearDownSummer.css';
 
-interface Props {}
 
 export default function TearDownSummery() {
   return (
@@ -19,66 +16,6 @@ export default function TearDownSummery() {
       </PDFDownloadLink>
     </div>
   );
-
-//   function MyPdfAppComponent(){
-//     const [dataForPdf, setDataForPdf] = React.useState()
-//     React.useEffect(()=>{
-//        goFetchTheData().then(res=> setDataForPdf(res))
-//     },[])
-//     return{
-//        dataForPdf && (
-//        <PDFViewer>
-//           <MyDocument data={dataForPdf}/>
-//        </PDFViewer>
-//        )
-//     }
-//  }
-
-  // const testData = {
-  //   test: 'test',
-  //   data: {
-  //     data1: 'data1',
-  //     data2: 'data2'
-  //   }
-  // };
-
-  // const testBtn = (event: any) => {
-  //   console.log('Submit Form Btn Pressed, event:', event);
-  // };
-
-  // return (
-  //   <div className={styles['form-container']}>
-  //     <div className={styles.page}>
-  //       <div>
-  //         <div>Header Test</div>
-  //       </div>
-  //       <div>
-  //         <div>Body Test</div>
-  //       </div>
-  //       <div>
-  //         <div>Footer Test</div>
-  //       </div>
-  //     </div>
-  //     <div>
-  //       <Btn buttonName="Submit Form" ClickHandler={testBtn} />
-  //     </div>
-
-  //     <PDFDownloadLink
-  //       document={<TearDownPDF data={testData} />}
-  //       fileName="new.pdf"
-  //       style={{
-  //         textDecoration: 'none',
-  //         padding: '10px',
-  //         color: 'blue',
-  //         backgroundColor: '#f2f2f2',
-  //         border: '1px solid black',
-  //         borderRadius: '5px'
-  //       }}
-  //     >
-  //       Generate
-  //     </PDFDownloadLink>
-  //   </div>
-  // );
 }
 
 ReactDOM.render(<TearDownSummery />, document.getElementById('root'));
