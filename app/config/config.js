517e089c02343fff056ebfbd9cf3a7a582ddbc27
Matthew Staniszewski <MStaniszewski@aeroparts.aero>
2020-08-11 14:29:53 -0600

    Removed commented section on config/config

--- app/config/config.js
+++ app/config/config.js
@@ -7,15 +7,4 @@ const config = {
 
 const pool = new sql.ConnectionPool(config);
 
-// const pool = new sql.ConnectionPool({
-//   driver: 'msnodesqlv8',
-//   server: `AMR-FS1\\SQLEXPRESS`,
-//   database: 'Repair',
-//   // Add when one can use the windows cred
-//   options: {
-//     trustedConnection: true,
-//     useUTC: true
-//   }
-// });
-
 module.exports = pool;
