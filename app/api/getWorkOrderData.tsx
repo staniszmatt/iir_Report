d3ec30b86960174b54de8704be07a9f4eb9b74ef
Matthew Staniszewski <MStaniszewski@aeroparts.aero>
2020-08-24 09:47:40 -0600

    Fixed Query on getWorkOrderData to stop excessive wait time on DB response

--- app/api/getWorkOrderData.tsx
+++ app/api/getWorkOrderData.tsx
@@ -23,16 +23,16 @@ async function getWorkOrderData(request: Request) {
 
   console.log('request: ', request);
 
-  console.log('query: *** *** ***\n', `SELECT sales_order.SalesOrderNumber, sales_order_line.SalesOrderAndLineNumber, sales_order_line.ItemNumber, sales_order_line.PartNumber, sales_order_line.PartDescription, sales_order_line.SerialNumber, sales_order_line.Quantity, sales_order_line.TSN, sales_order_line.TSR, sales_order_line.TSO,
-  sales_order.CustomerNumber, sales_order.CustomerName, sales_order.CustomerOrderNumber, sales_order.DateIssuedYYMMDD, sales_order.Warrenty_Y_N, sales_order.OrderType
+  console.log('query: *** *** ***\n', `SELECT sales_order_line.SalesOrderAndLineNumber, sales_order_line.ItemNumber, sales_order_line.PartNumber, sales_order_line.PartDescription, sales_order_line.SerialNumber, sales_order_line.Quantity, sales_order_line.TSN, sales_order_line.TSR, sales_order_line.TSO,
+  sales_order.SalesOrderNumber, sales_order.CustomerNumber, sales_order.CustomerName, sales_order.CustomerOrderNumber, sales_order.DateIssuedYYMMDD, sales_order.Warrenty_Y_N, sales_order.OrderType
   FROM sales_order_line
   INNER JOIN sales_order ON sales_order_line.SalesOrderNumber = sales_order.SalesOrderNumber
   WHERE sales_order_line.SalesOrderNumber = '${request.workOrderSearch}' AND sales_order_line.ItemNumber = '${request.workOrderSearchLineItem}'`)
 
   try {
     const db = await odbc.connect('DSN=AeroSuper');
-    const data = await db.query(`SELECT sales_order.SalesOrderNumber, sales_order_line.SalesOrderAndLineNumber, sales_order_line.ItemNumber, sales_order_line.PartNumber, sales_order_line.PartDescription, sales_order_line.SerialNumber, sales_order_line.Quantity, sales_order_line.TSN, sales_order_line.TSR, sales_order_line.TSO,
-    sales_order.CustomerNumber, sales_order.CustomerName, sales_order.CustomerOrderNumber, sales_order.DateIssuedYYMMDD, sales_order.Warrenty_Y_N, sales_order.OrderType
+    const data = await db.query(`SELECT sales_order_line.SalesOrderAndLineNumber, sales_order_line.ItemNumber, sales_order_line.PartNumber, sales_order_line.PartDescription, sales_order_line.SerialNumber, sales_order_line.Quantity, sales_order_line.TSN, sales_order_line.TSR, sales_order_line.TSO,
+    sales_order.SalesOrderNumber, sales_order.CustomerNumber, sales_order.CustomerName, sales_order.CustomerOrderNumber, sales_order.DateIssuedYYMMDD, sales_order.Warrenty_Y_N, sales_order.OrderType
     FROM sales_order_line
     INNER JOIN sales_order ON sales_order_line.SalesOrderNumber = sales_order.SalesOrderNumber
     WHERE sales_order_line.SalesOrderNumber = '${request.workOrderSearch}' AND sales_order_line.ItemNumber = '${request.workOrderSearchLineItem}'`);
@@ -42,7 +42,7 @@ async function getWorkOrderData(request: Request) {
       // Can't get the server to do more than one join for some reason, work around is a second query.
       const secondData: any = await db.query(`SELECT traveler_header.Manual_Combined, traveler_header.Work_Order_Number, traveler_header.Trv_Num
       FROM traveler_header
-      WHERE traveler_header.Sales_Order_Number = '${request.workOrderSearch}' AND WHERE traveler_header.Sales_Order_Line_Item = '${request.workOrderSearchLineItem}'`);
+      WHERE traveler_header.Work_Order_Number = '${request.workOrderSearch}' AND traveler_header.Sales_Order_Line_Item = '${request.workOrderSearchLineItem}'`);
 
       const { Manual_Combined, Work_Order_Number, Trv_Num } = secondData[0];
 
