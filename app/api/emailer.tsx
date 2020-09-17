81b8f8b0a75c3767f55ce2bf3e017385bf8e05bb
Matthew Staniszewski <MStaniszewski@aeroparts.aero>
2020-09-17 10:23:22 -0600

    Updated Final Email setup Setup the NODEMON emailer for auto emailing in actions/emailer. Removed the Secondary option comments since it isn't as secure. Added file template of getting the password for email in the app/config folder

--- app/api/emailer.tsx
+++ app/api/emailer.tsx
@@ -1,6 +1,7 @@
+import getPass from '../config/configEmail'
+
 const userName = require('username');
 const nodemailer = require('nodemailer');
-// const sendmail = require('sendmail')();
 
 interface Request {
   testInfo: {
@@ -31,6 +32,7 @@ async function emailer(request: Request) {
     infoResp: {},
     user: ''
   };
+  const emailPass = getPass();
 
   try {
     const user = await userName();
@@ -41,30 +43,15 @@ async function emailer(request: Request) {
       port: 587,
       secure: false,
       auth: {
-        user: `mstaniszewski@aeroparts.aero`,
-        pass: 'Izybell199*'
+        user: `NODEMON@aeroparts.aero`,
+        pass: `${emailPass}`
       }
     });
 
     const info = await transporter.sendMail({
-      from: '"My Self" <mstaniszewski@aeroparts.aero>',
+      from: '"NODEMON" <NODEMON@aeroparts.aero>',
       to: 'mstaniszewski@aeroparts.aero',
       subject: `TEAR DOWN UPDATE: ${CustomerName}, WO: ${workOrder}, PN: ${PartNumber}`,
-      // text: `Text Tear Down has been updated by: ${user} For WO: ${workOrder}. \n
-      //   \n
-      //   Current notes: \n
-      //   \n
-      //   \t Reason for Removal: \n
-      //   ${customerReasonForRemoval} \n
-      //   \n
-      //   \t General Condition: \n
-      //   ${genConditionReceived} \n
-      //   \n
-      //   \t Evaluation Findings: \n
-      //   ${evalFindings} \n
-      //   \n
-      //   \t Worked Performed: \n
-      //   ${workedPerformed}`
       html: `<!DOCUMENT html>
       <html>
         <head></head>
@@ -90,18 +77,6 @@ async function emailer(request: Request) {
       </html>
       `
     });
-
-    // TODO: For none logging in emailing, restricted tho.
-    // TODO: This is working but requires email settings to be trusted.
-    // const info = await sendmail({
-    //   // from: '"NODEMON" <no-reply@aeroparts.aero>',
-    //   from: '"Matthew Staniszewski" <mstaniszewski@aeroparts.aero>',
-    //   to: 'mstaniszewski@aeroparts.aero',
-    //   subject: 'Test Auto Email',
-    //   text: 'Testing Auto Emailer'
-    //   // Link not good because the updates do not update pdf.
-    //   // html: '<p>CLICK <a href="T:CPLT_TRAVELERS\\TearDowns\\CN101-01_TEAR_DOWN.pdf"> HERE </a> to open PDF</p>'
-    // });
 
     if (info.rejected.length === 0) {
       returnData.infoResp = info;
