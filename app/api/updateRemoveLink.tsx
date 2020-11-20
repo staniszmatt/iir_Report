9c8bb4f4ecb137b8bc8ef251fae23df67bc35460
staniszmatt <matthewpstaniszewski@gmail.com>
2020-11-20 15:06:15 -0700

    Updated version 1.3.1 REmoved un-used comments on updateRemoveLink. Updated getWorkOrderData with dev tables

--- app/api/updateRemoveLink.tsx
+++ app/api/updateRemoveLink.tsx
@@ -72,49 +72,6 @@ async function updateRemoveLink(request: Request) {
     if (linkAPEData.recordset[0].id) {
       returnData.resp.linkAPEData = linkAPEData;
       returnData.succuss = true;
-
-      // try {
-      //   const preStateWO = await new sql.PreparedStatement(db);
-      //   preStateWO.input('ape', sql.VarChar(12));
-      //   preStateWO.input('wo', sql.VarChar(12));
-      //   preStateWO.input('line', sql.VarChar(2));
-      //   const queryStringWO = `
-      //   IF EXISTS (SELECT * FROM tear_down_notes_dev WHERE SalesOrderNumber = @wo AND tear_down_notes_dev.salesOrderNumberLine = @line)
-      //   BEGIN
-      //     UPDATE tear_down_notes_dev
-      //       SET linkedAPEWorkOrder = @ape
-      //         OUTPUT inserted.id, GETDATE() as dateStamp, CURRENT_USER as userName, HOST_NAME() AS hostName
-      //           WHERE tear_down_notes_dev.SalesOrderNumber = @wo AND tear_down_notes_dev.salesOrderNumberLine = @line
-      //   END
-      //   ELSE
-      //     BEGIN
-      //       INSERT INTO tear_down_notes_dev (SalesOrderNumber, linkedAPEWorkOrder, salesOrderNumberLine)
-      //         OUTPUT inserted.id, GETDATE() as dateStamp, CURRENT_USER as userName, HOST_NAME() AS hostName
-      //       VALUES (@wo, @ape, @line)
-      //   END`;
-
-      //   await preStateWO.prepare(queryStringWO);
-      //   const linkWOData = await preStateWO.execute(preAPEStateParams);
-      //   await preStateWO.unprepare();
-
-      //   if (linkWOData.recordset[0].id) {
-      //     returnData.succuss = true;
-      //     returnData.resp.linkWOData = linkWOData;
-      //   } else {
-      //     returnData.succuss = true;
-      //     returnData.resp.linkWOData = linkWOData;
-      //     returnData.error = {
-      //       errorMsg: 'Something went wrong adding IIR report!'
-      //     };
-      //   }
-      // } catch (error) {
-      //   // Not sure if there is a better way but don't need to return the array of key value pairs.
-      //   // eslint-disable-next-line array-callback-return
-      //   Object.getOwnPropertyNames(error).map(key => {
-      //     // eslint-disable-next-line no-useless-return
-      //     returnData.error[key] = error[key];
-      //   });
-      // }
     } else {
       returnData.succuss = true;
       returnData.resp.linkAPEData = linkAPEData;
